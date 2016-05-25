197c9b91617603d3940c990ce8fec0b811df06c8
Chris Cannam <c.cannam@qmul.ac.uk>
2016-05-25 14:37:32 +0100

    Test file

---
+++ test/test-vampipe-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+( bin/vampipe-convert request -i json -o capnp |
+	VAMP_PATH=./vamp-plugin-sdk/examples bin/vampipe-server |
+	bin/vampipe-convert response -i capnp -o json ) <<EOF
+{"type":"list","content": {}}
+{"type":"load","content": {"pluginKey":"vamp-example-plugins:percussiononsets","inputSampleRate":44100,"adapterFlags":["AdaptInputDomain","AdaptBufferSize"]}}
+{"type":"configure","content":{"pluginHandle":1,"configuration":{"blockSize": 8, "channelCount": 1, "parameterValues": {"sensitivity": 40, "threshold": 3}, "stepSize": 8}}}
+{"type":"process","content": {"pluginHandle": 1, "processInput": { "timestamp": {"s": 0, "n": 0}, "inputBuffers": [{"values": [1,2,3,4,5,6,7,8]}]}}}
+{"type":"finish","content": {"pluginHandle": 1}}
+EOF
+
